1908022bd39003dfa9943dd97ef6601ef2014204
mozzi327 <80810465+mozzi327@users.noreply.github.com>
2022-12-22 21:36:06 +0900

    Member 도메인 엔티티 작성 (#8)
    * feat: Member Entity 추가 및 Spring Boot 버전 변경
    
    * feat: Member 도메인 엔티티 작성

---
+++ server/src/main/java/com/codueon/boostUp/domain/member/entity/MemberStatus.java
@@ -0,0 +1,7 @@
+package com.codueon.boostUp.domain.member.entity;
+
+public enum MemberStatus {
+    MEMBER_ACTIVE,
+    MEMBER_SLEEP,
+    MEMBER_EXITED
+}
---
+++ server/src/main/java/com/codueon/boostUp/domain/member/utils/OAuthConstant.java
@@ -0,0 +1,8 @@
+package com.codueon.boostUp.domain.member.utils;
+
+public class OAuthConstant {
+    public static final String GOOGLE = "google";
+    public static final String KAKAO = "kakao";
+    public static final String NAVER = "naver";
+
+}
