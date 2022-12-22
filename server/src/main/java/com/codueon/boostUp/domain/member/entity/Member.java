afec61d4e354b4e5db2cb0539698073b41ce831d
mozzi327 <80810465+mozzi327@users.noreply.github.com>
2022-12-22 21:36:06 +0900

    Member 도메인 엔티티 작성 (#8)
    * feat: Member Entity 추가 및 Spring Boot 버전 변경
    
    * feat: Member 도메인 엔티티 작성

--- server/src/main/java/com/codueon/boostUp/domain/member/entity/Member.java
+++ server/src/main/java/com/codueon/boostUp/domain/member/entity/Member.java
@@ -5,7 +5,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,8 +21,18 @@ public class Member {
     private String name;
     private String address;
     private String company;
+
     @ElementCollection(fetch = FetchType.EAGER)
     private List<String> roles = new ArrayList<>();
+
+    @Enumerated(value = EnumType.STRING)
+    private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
+
+    @Enumerated(value = EnumType.STRING)
+    private AccountStatus accountStatus = AccountStatus.COMMON_MEMBER;
+
+    @OneToOne(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private MemberImage memberImage;
 
     @Builder
     public Member(Long id,
@@ -40,5 +49,14 @@ public class Member {
         this.address = address;
         this.company = company;
         this.roles = roles;
+    }
+
+    public void setMemberStatus(MemberStatus memberStatus) {
+        this.memberStatus = memberStatus;
+    }
+
+    public void addMemberImage(MemberImage memberImage) {
+        if (memberImage.getMember() != this) memberImage.setMember(this);
+        this.memberImage = memberImage;
     }
 }
