d4bfe10bd6a09f03a42165cdcddcf769eda54e2a
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2022-12-22 23:10:27 +0900

    feat: Lesson Entity 작성 (#12)

--- server/src/main/java/com/codueon/boostUp/domain/lesson/entity/Address.java
+++ server/src/main/java/com/codueon/boostUp/domain/lesson/entity/Address.java
@@ -1,9 +1,10 @@
 package com.codueon.boostUp.domain.lesson.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,5 +12,16 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ADDRESS_ID")
+    private Long id;
+    private String address;
+    @OneToMany(mappedBy = "lesson", cascade = CascadeType.ALL)
     private List<LessonAddress> lessonAddresses = new ArrayList<>();
+    @Builder
+    public Address(Long id, String address) {
+        this.id = id;
+        this.address = address;
+    }
 }
--- server/src/main/java/com/codueon/boostUp/domain/lesson/entity/Language.java
+++ server/src/main/java/com/codueon/boostUp/domain/lesson/entity/Language.java
@@ -1,9 +1,10 @@
 package com.codueon.boostUp.domain.lesson.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,5 +12,16 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class Language {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "LANGUAGE_ID")
+    private Long id;
+    private String language;
+    @OneToMany(mappedBy = "lesson", cascade = CascadeType.ALL)
     private List<LessonLanguage> lessonLanguages = new ArrayList<>();
+    @Builder
+    public Language(Long id, String language) {
+        this.id = id;
+        this.language = language;
+    }
 }
