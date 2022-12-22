67bec12962d5e100d04b5a49773dd5f6dbaa31bd
LeeGoh <gyuleegoh@gmail.com>
2022-12-22 21:48:27 +0900

    feat: Review Entity 작성 및 Bookmark Entity 수정

--- server/src/main/java/com/codueon/boostUp/domain/bookmark/entity/Bookmark.java
+++ server/src/main/java/com/codueon/boostUp/domain/bookmark/entity/Bookmark.java
@@ -1,10 +1,15 @@
 package com.codueon.boostUp.domain.bookmark.entity;
 
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Bookmark {
 
     @Id
