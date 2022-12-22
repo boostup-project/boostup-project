37792e03ed4a7ae9ea0530507ae15933f82a38ed
quartz614 <quartz614@naver.com>
2022-12-22 15:23:45 +0900

    chore : 초기 세팅

---
+++ server/src/main/java/com/codueon/boostUp/BoostUpApplication.java
@@ -0,0 +1,13 @@
+package com.codueon.boostUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoostUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoostUpApplication.class, args);
+	}
+
+}
