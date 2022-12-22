2e31770b625545bfd8ea65906fc446fa3cf7b428
mozzi327 <mozzi327@gmail.com>
2022-12-22 23:21:45 +0900

    feat: global exception 및 디스코드 webhook 추가

---
+++ server/src/main/java/com/codueon/boostUp/global/webhook/SendErrorToDiscord.java
@@ -0,0 +1,52 @@
+package com.codueon.boostUp.global.webhook;
+
+import com.codueon.boostUp.domain.member.exception.AuthException;
+import com.codueon.boostUp.global.exception.BusinessLogicException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class SendErrorToDiscord {
+    private final WebHookFeign webHookFeign;
+
+    public void sendAuthExceptionToDiscord(AuthException e) {
+        WebHookError webHookError = WebHookError.builder()
+                .content(e.getMessage() + " error occurred \n")
+                .embeds(makeErrorList(e))
+                .tts(false)
+                .build();
+        webHookFeign.sendServiceLogging("application/json", webHookError);
+    }
+
+    public void sendBusinessExceptionToDiscord(BusinessLogicException e) {
+        WebHookError webHookError = WebHookError.builder()
+                .content(e.getMessage() + " error occurred \n")
+                .embeds(makeErrorList(e))
+                .tts(false)
+                .build();
+        webHookFeign.sendServiceLogging("application/json", webHookError);
+    }
+
+    public void sendServerExceptionToDiscord(Exception e) {
+        WebHookError webHookError = WebHookError.builder()
+                .content("server error occurred \n")
+                .embeds(makeErrorList(e))
+                .tts(false)
+                .build();
+        webHookFeign.sendServerLogging("application/json", webHookError);
+    }
+
+    private List<ServiceError> makeErrorList(Exception... exceptions) {
+        return Arrays.stream(exceptions).map(
+                e -> ServiceError.builder()
+                        .title(e.getMessage())
+                        .description(e.toString())
+                        .build()).collect(Collectors.toList());
+    }
+}
