64e5c4223958027dc0cd16089f13256cf28bc30a
mozzi327 <80810465+mozzi327@users.noreply.github.com>
2022-12-23 14:28:36 +0900

    feat: reviewDto 작성 (#25)
    * fix: languages 엔티티 충돌 수정
    
    * feat: reviewDto 작성

---
+++ server/src/main/java/com/codueon/boostUp/domain/reveiw/dto/ReviewResponse.java
@@ -0,0 +1,33 @@
+package com.codueon.boostUp.domain.reveiw.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class ReviewResponse {
+    private Long reviewId;
+    private String image;
+    private String name;
+    private Double score;
+    private String comment;
+    private LocalDateTime createdAt;
+
+    @Builder
+    public ReviewResponse(Long reviewId,
+                          String image,
+                          String name,
+                          Double score,
+                          String comment,
+                          LocalDateTime createdAt) {
+        this.reviewId = reviewId;
+        this.image = image;
+        this.name = name;
+        this.score = score;
+        this.comment = comment;
+        this.createdAt = createdAt;
+    }
+}
