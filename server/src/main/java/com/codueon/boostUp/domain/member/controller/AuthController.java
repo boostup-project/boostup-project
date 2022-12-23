93aa20016c85db4b8189dd985a6f79ddde4d1498
mozzi327 <mozzi327@gmail.com>
2022-12-23 16:21:59 +0900

    feat: memberController, AuthController 뼈대 작성

---
+++ server/src/main/java/com/codueon/boostUp/domain/member/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.codueon.boostUp.domain.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.codueon.boostUp.domain.member.dto.PostLogin;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @PostMapping("/login")
+    public ResponseEntity postLoginMember(@RequestBody PostLogin login) {
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity deleteLoginMember() {
+        // TODO : 시큐리티 적용 시 Authentication 객체 추가 요
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/re-issue")
+    public ResponseEntity getReIssueToken(@RequestHeader("authorization") String accessToken,
+                                          @RequestHeader("refreshToken") String refreshToken) {
+        // TODO : 시큐리티 적용 시 Authentication 객체 추가 요
+        return ResponseEntity.ok().build();
+    }
+}
