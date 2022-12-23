e78406f81b83b05adbffb9583ce774c5e1006026
quartz614 <quartz614@naver.com>
2022-12-23 15:06:47 +0900

    refatcor: GetDetailLesson DTO 생성자 리펙토링

--- server/src/main/java/com/codueon/boostUp/domain/lesson/dto/GetDetailLesson.java
+++ server/src/main/java/com/codueon/boostUp/domain/lesson/dto/GetDetailLesson.java
@@ -20,4 +20,31 @@ public class GetDetailLesson {
     private String curriculum;
     private Boolean isBookmark;
 
+    public GetDetailLesson(String title,
+                           Integer cost,
+                           String profileImage,
+                           String name,
+                           String company,
+                           String introduction,
+                           String language,
+                           String career,
+                           String address,
+                           String personality,
+                           String images,
+                           String curriculum,
+                           Boolean isBookmark) {
+        this.title = title;
+        this.cost = cost;
+        this.profileImage = profileImage;
+        this.name = name;
+        this.company = company;
+        this.introduction = introduction;
+        this.language = language;
+        this.career = career;
+        this.address = address;
+        this.personality = personality;
+        this.images = images;
+        this.curriculum = curriculum;
+        this.isBookmark = isBookmark;
+    }
 }
