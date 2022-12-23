396c4efcfd150275c61d1efd64f8c1eb49be3be4
mozzi327 <80810465+mozzi327@users.noreply.github.com>
2022-12-23 15:08:50 +0900

    refactor: ReviewResponse -> GetReview 명명 수정 (#30)
    * fix: languages 엔티티 충돌 수정
    
    * refactor: ReviewResponse -> GetReview 명명 수정

--- server/src/main/java/com/codueon/boostUp/domain/reveiw/dto/GetReview.java
+++ server/src/main/java/com/codueon/boostUp/domain/reveiw/dto/GetReview.java
@@ -3,12 +3,11 @@ package com.codueon.boostUp.domain.reveiw.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
-public class ReviewResponse {
+public class GetReview {
     private Long reviewId;
     private String image;
     private String name;
@@ -17,12 +16,12 @@ public class ReviewResponse {
     private LocalDateTime createdAt;
 
     @Builder
-    public ReviewResponse(Long reviewId,
-                          String image,
-                          String name,
-                          Double score,
-                          String comment,
-                          LocalDateTime createdAt) {
+    public GetReview(Long reviewId,
+                     String image,
+                     String name,
+                     Double score,
+                     String comment,
+                     LocalDateTime createdAt) {
         this.reviewId = reviewId;
         this.image = image;
         this.name = name;
