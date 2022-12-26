455577fb2baec1a2438568fa313651e3ce0ddaa2
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2022-12-26 21:27:59 +0900

    feat: postSuggest 과외 신청 요청 기능 구현 (#61)
    Co-authored-by: LeeGoh <“gyuleegoh@gmail.com”>

--- server/src/main/java/com/codueon/boostUp/domain/suggest/service/SuggestService.java
+++ server/src/main/java/com/codueon/boostUp/domain/suggest/service/SuggestService.java
@@ -1,6 +1,7 @@
 package com.codueon.boostUp.domain.suggest.service;
 
 import com.codueon.boostUp.domain.suggest.dto.PostSuggest;
+import com.codueon.boostUp.domain.suggest.entity.Suggest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,18 @@ public class SuggestService {
     @Transactional
     public void createSuggest(PostSuggest post, Long lessonId, Long memberId) {
 //        Lesson findLesson = lessonDbService.ifExistsReturnLesson(lessonId);
+
+//        if(memberId.equals(findLesson.getMemberId())) {
+//            throw new BusinessLogicException(ExceptionCode.HOST_CANNOT_RESERVATION);
+//        }
+
+        Suggest suggest = Suggest.builder()
+                .days(post.getDays())
+                .languages(post.getLanguages())
+                .requests(post.getRequests())
+                .build();
+
+        suggestDbService.saveSuggest(suggest);
     }
 
 }
