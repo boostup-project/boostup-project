16be89b5f327109940b8b84404ce158e1d957513
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2022-12-26 22:03:22 +0900

    test: postSuggest 과외 신청하기 테스트 코드 작성 (#63)
    Co-authored-by: LeeGoh <“gyuleegoh@gmail.com”>

---
+++ server/src/test/java/com/codueon/boostUp/domain/suggest/contoller/SuggestControllerTest.java
@@ -0,0 +1,64 @@
+package com.codueon.boostUp.domain.suggest.contoller;
+
+import com.codueon.boostUp.domain.member.entity.Member;
+import com.codueon.boostUp.domain.suggest.entity.Suggest;
+import com.codueon.boostUp.domain.suggest.service.SuggestDbService;
+import com.codueon.boostUp.domain.suggest.service.SuggestService;
+import com.codueon.boostUp.global.exception.GlobalAdvice;
+import com.codueon.boostUp.global.webhook.SendErrorToDiscord;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+@AutoConfigureMockMvc
+@MockBean(JpaMetamodelMappingContext.class)
+@WebMvcTest(controllers = {SuggestController.class})
+class SuggestControllerTest {
+
+    @Autowired
+    protected Gson gson;
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @MockBean
+    protected GlobalAdvice globalAdvice;
+
+    @MockBean
+    protected SuggestService suggestService;
+
+    @MockBean
+    protected SuggestDbService suggestDbService;
+
+    @MockBean
+    protected SendErrorToDiscord sendErrorToDiscord;
+
+    protected Member member;
+    protected Suggest suggest;
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+        member = Member.builder()
+                .id(1L)
+                .name("김길동")
+                .email("gddong@gmail.com")
+                .roles(List.of("USER"))
+                .build();
+
+        suggest = Suggest.builder()
+                .id(1L)
+                .days("월, 수, 금")
+                .languages("Java")
+                .requests("누워서 수업 들어도 되나요?")
+                .build();
+
+    }
+}
