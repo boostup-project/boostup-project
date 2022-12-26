bf49c3850696c608e07ae2b1531e2ff27e4c029e
mozzi327 <80810465+mozzi327@users.noreply.github.com>
2022-12-26 20:17:55 +0900

    chore: chat 기능 구현 사항 보류 (#59)
    * fix: languages 엔티티 충돌 수정
    
    * feat: 북마크 기능 구현 중
    
    * chore: chat 기능 구현 사항 보류

--- server/src/main/java/com/codueon/boostUp/domain/chat/repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codueon.boostUp.domain.chat.repository;
-
-import com.codueon.boostUp.domain.chat.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface RoomRepository extends JpaRepository<ChatRoom, Long> {
-    List<ChatRoom> findByMerChantIdOrStudentId(Long merchantId, Long studentId);
-}
