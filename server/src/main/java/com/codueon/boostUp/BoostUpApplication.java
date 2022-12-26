f20126abc5cd3c89ed8e5cedce4b046299334a72
mozzi327 <80810465+mozzi327@users.noreply.github.com>
2022-12-26 17:24:46 +0900

    feat: chatting 기능 controller - service - repository 구현중 (#53)
    * fix: languages 엔티티 충돌 수정
    
    * feat: chatting 기능 controller - service - repository 구현중

--- server/src/main/java/com/codueon/boostUp/BoostUpApplication.java
+++ server/src/main/java/com/codueon/boostUp/BoostUpApplication.java
@@ -3,7 +3,11 @@ package com.codueon.boostUp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
+@EnableJpaAuditing
 @EnableFeignClients
 @SpringBootApplication
 public class BoostUpApplication {
---
+++ server/src/main/java/com/codueon/boostUp/domain/chat/dto/res/GetChatMember.java
@@ -0,0 +1,20 @@
+package com.codueon.boostUp.domain.chat.dto.res;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GetChatMember {
+    private Long memberId;
+    private String profileImage;
+    private String name;
+
+    @Builder
+    public GetChatMember(Long memberId, String profileImage, String name) {
+        this.memberId = memberId;
+        this.profileImage = profileImage;
+        this.name = name;
+    }
+}
