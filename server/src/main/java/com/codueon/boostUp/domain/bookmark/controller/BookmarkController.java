2149a37b68accb3cfc99c117ad741fa7c71317f9
mozzi327 <80810465+mozzi327@users.noreply.github.com>
2022-12-27 16:04:11 +0900

    test: 북마크 테스팅(create/delete) (#69)
    * fix: languages 엔티티 충돌 수정
    
    * test: 북마크 테스팅(create/delete)

--- server/src/main/java/com/codueon/boostUp/domain/bookmark/controller/BookmarkController.java
+++ server/src/main/java/com/codueon/boostUp/domain/bookmark/controller/BookmarkController.java
@@ -12,19 +12,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/bookmark")
 public class BookmarkController {
     private final BookmarkService bookmarkService;
+
     @GetMapping("/lesson/{lesson-id}")
-    public ResponseEntity changeBookmarks(@PathVariable("lesson-id") Long lessonId) {
+    public ResponseEntity changeBookmark(@PathVariable("lesson-id") Long lessonId) {
         // TODO : 시큐리티 적용 시 Authentication 객체 추가 요
         Long memberId = 1L; // 임시 하드 코딩 ㅋㅋ
-        bookmarkService.changeBookmarkStatus(memberId, lessonId);
-        return ResponseEntity.ok().build();
+        boolean isBookmarked = bookmarkService.changeBookmarkStatus(memberId, lessonId);
+        return ResponseEntity.ok().body(isBookmarked);
     }
 
     @GetMapping
---
+++ server/src/test/java/com/codueon/boostUp/domain/bookmark/controller/BookmarkGetTest.java
@@ -0,0 +1,14 @@
+package com.codueon.boostUp.domain.bookmark.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BookmarkGetTest extends BookmarkControllerTest {
+
+    @Test
+    @DisplayName("GET 북마크 전체 조회")
+    void getBookmarks() throws Exception {
+
+
+    }
+}
