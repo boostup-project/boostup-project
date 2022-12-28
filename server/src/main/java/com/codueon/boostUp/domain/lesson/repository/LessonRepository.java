3aad7e85a0bcb44cd50ebbc0fed524c2f6cc6a3c
quartz614 <quartz614@naver.com>
2022-12-28 14:09:54 +0900

    feat : S3 Server 환경 구성 중

---
+++ server/src/main/java/com/codueon/boostUp/domain/lesson/repository/LessonRepository.java
@@ -0,0 +1,7 @@
+package com.codueon.boostUp.domain.lesson.repository;
+
+import com.codueon.boostUp.domain.lesson.entity.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LessonRepository extends JpaRepository<Lesson, Long>, CustomLessonRepository{
+}
---
+++ server/src/main/java/com/codueon/boostUp/domain/lesson/service/LessonService.java
@@ -0,0 +1,12 @@
+package com.codueon.boostUp.domain.lesson.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LessonService {
+    private final LessonDbService lessonDbService;
+
+
+}
