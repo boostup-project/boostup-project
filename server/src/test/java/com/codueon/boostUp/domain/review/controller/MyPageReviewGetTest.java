97081105e494a07ff45a10e779e1d70b09c4bd8a
mozzi327 <80810465+mozzi327@users.noreply.github.com>
2022-12-29 14:50:21 +0900

    test: bookmarkController Test 1차 완료 (#102)

--- server/src/test/java/com/codueon/boostUp/domain/review/controller/MyPageReviewGetTest.java
+++ server/src/test/java/com/codueon/boostUp/domain/review/controller/MyPageReviewGetTest.java
@@ -8,11 +8,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
-
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
