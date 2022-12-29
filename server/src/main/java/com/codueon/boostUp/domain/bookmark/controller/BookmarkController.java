2a6ddce5a624475bf81c15ea72d782f20e36acbd
mozzi327 <80810465+mozzi327@users.noreply.github.com>
2022-12-29 13:45:43 +0900

    refactor: Bookmark API 분리 수정 (#98)

--- server/src/main/java/com/codueon/boostUp/domain/bookmark/controller/BookmarkController.java
+++ server/src/main/java/com/codueon/boostUp/domain/bookmark/controller/BookmarkController.java
@@ -3,6 +3,7 @@ package com.codueon.boostUp.domain.bookmark.controller;
 import com.codueon.boostUp.domain.bookmark.dto.GetBookmark;
 import com.codueon.boostUp.domain.bookmark.entity.Bookmark;
 import com.codueon.boostUp.domain.bookmark.service.BookmarkService;
+import com.codueon.boostUp.domain.dto.MultiResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookmarkController {
     private final BookmarkService bookmarkService;
 
+    /**
+     * 사용자 북마크 여부 조회 컨트롤러 메서드
+     * @param lessonId 과외 식별자
+     * @return Boolean
+     * @author mozzi327
+     */
+    @GetMapping("/lesson/{lesson-id}")
+    public ResponseEntity getBookmark(@PathVariable("lesson-id") Long lessonId) {
+        // TODO : 시큐리티 적용 시 Authentication 객체 추가 요
+        Long memberId = 1L; // 임시 하드 코딩 ㅋㅋ
+        boolean isBookmarked = bookmarkService.isMemberBookmarked(memberId, lessonId);
+        return ResponseEntity.ok().body(isBookmarked);
+    }
+
+    /**
+     * 북마크 추가 / 삭제 컨트롤러 메서드
+     * @param lessonId 과외 식별자
+     * @return isBookmark
+     * @author mozzi327
+     */
     @GetMapping("/lesson/{lesson-id}/edit")
     public ResponseEntity changeBookmark(@PathVariable("lesson-id") Long lessonId) {
         // TODO : 시큐리티 적용 시 Authentication 객체 추가 요
@@ -26,11 +47,16 @@ public class BookmarkController {
         return ResponseEntity.ok().body(isBookmarked);
     }
 
+    /**
+     * 마이페이지 북마크 리스트 조회 컨트롤러 메서드
+     * @param pageable 페이지 정보
+     * @author mozzi327
+     */
     @GetMapping
     public ResponseEntity getBookmarks(Pageable pageable) {
         // TODO : 시큐리티 적용 시 Authentication 객체 추가 요
         Long memberId = 1L; // 임시 하드 코딩 ㅋㅋ
         Page<GetBookmark> bookmarkList = bookmarkService.findBookmarkList(memberId, pageable);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body(new MultiResponseDto<>(bookmarkList));
     }
 }
--- server/src/main/java/com/codueon/boostUp/domain/bookmark/service/BookmarkService.java
+++ server/src/main/java/com/codueon/boostUp/domain/bookmark/service/BookmarkService.java
@@ -18,9 +18,30 @@ import java.util.Optional;
 public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
 
+    /**
+     * 북마크 여부 조회 메서드
+     * @param memberId 사용자 식별자
+     * @param lessonId 과외 식별자
+     * @return isBookmark
+     * @author mozzi327
+     */
+    public boolean isMemberBookmarked(Long memberId, Long lessonId) {
+        boolean isBookmark = true;
+        Optional<Bookmark> findBookmark = bookmarkRepository.findByMemberIdAndLessonId(memberId, lessonId);
+        if (findBookmark.isEmpty()) isBookmark = false;
+        return isBookmark;
+    }
+
+    /**
+     * 북마크 상태 변경 메서드(추가/삭제)
+     * @param memberId 사용자 식별자
+     * @param lessonId 과외 식별자
+     * @return isBookmark
+     * @author mozzi327
+     */
     public boolean changeBookmarkStatus(Long memberId, Long lessonId) {
         Optional<Bookmark> findBookmark = bookmarkRepository.findByMemberIdAndLessonId(memberId, lessonId);
-        boolean isBookmarked = false;
+        boolean isBookmark = false;
         if (findBookmark.isPresent()) bookmarkRepository.delete(findBookmark.get());
         else {
             bookmarkRepository.save(
@@ -29,11 +50,18 @@ public class BookmarkService {
                             .lessonId(lessonId)
                             .build()
             );
-            isBookmarked = true;
+            isBookmark = true;
         }
-        return isBookmarked;
+        return isBookmark;
     }
 
+    /**
+     * 사용자 북마크 리스트 조회 메서드
+     * @param memberId 사용자 식별자
+     * @param pageable 페이지 정보
+     * @return Page(GetBookmark)
+     * @author mozzi327
+     */
     public Page<GetBookmark> findBookmarkList(Long memberId, Pageable pageable) {
         return bookmarkRepository.getBookmarkList(memberId, pageable);
     }
