49f2cf636975641b7b5e098a3b5f3d51eb28f693
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2022-12-30 16:30:33 +0900

    feat: getSuggestInfo 결제 화면 조회 기능 구현 및 SuggestDbService 수정 (#125)
    Co-authored-by: LeeGoh <“gyuleegoh@gmail.com”>

--- server/src/main/java/com/codueon/boostUp/domain/suggest/service/SuggestDbService.java
+++ server/src/main/java/com/codueon/boostUp/domain/suggest/service/SuggestDbService.java
@@ -1,10 +1,7 @@
 package com.codueon.boostUp.domain.suggest.service;
 
-import com.codueon.boostUp.domain.lesson.entity.Lesson;
-import com.codueon.boostUp.domain.lesson.service.LessonDbService;
 import com.codueon.boostUp.domain.suggest.dto.GetStudentSuggest;
 import com.codueon.boostUp.domain.suggest.dto.GetTeacherSuggest;
-import com.codueon.boostUp.domain.suggest.dto.PostReason;
 import com.codueon.boostUp.domain.suggest.entity.Reason;
 import com.codueon.boostUp.domain.suggest.entity.Suggest;
 import com.codueon.boostUp.domain.suggest.repository.ReasonRepository;
@@ -22,15 +19,22 @@ public class SuggestDbService {
 
     private final SuggestRepository suggestRepository;
     private final ReasonRepository reasonRepository;
-    private final LessonDbService lessonDbService;
 
     public Suggest ifExistsReturnSuggest(Long suggestId) {
         return suggestRepository.findById(suggestId)
                 .orElseThrow(() -> new BusinessLogicException(ExceptionCode.RESERVATION_NOT_FOUND));
     }
 
-    public Suggest saveSuggest(Suggest suggest) {
-        return suggestRepository.save(suggest);
+    public void saveSuggest(Suggest suggest) {
+        suggestRepository.save(suggest);
+    }
+
+    public void saveReason(Reason reason) {
+        reasonRepository.save(reason);
+    }
+
+    public void deleteSuggest(Suggest suggest) {
+        suggestRepository.delete(suggest);
     }
 
     public Page<GetTeacherSuggest> getTeacherSuggestsOnMyPage(Long lessonId, Long memberId, int tabId, Pageable pageable) {
@@ -41,36 +45,4 @@ public class SuggestDbService {
         return suggestRepository.getStudentSuggestsOnMyPage(memberId, pageable);
     }
 
-    public void cancelSuggest(Long suggestId, Long memberId) {
-
-        Suggest findSuggest = ifExistsReturnSuggest(suggestId);
-
-        if (!memberId.equals(findSuggest.getMemberId())) {
-            throw new BusinessLogicException(ExceptionCode.INVALID_ACCESS);
-        }
-
-        if (!findSuggest.getStatus().equals(Suggest.SuggestStatus.ACCEPT_IN_PROGRESS) &&
-            !findSuggest.getStatus().equals(Suggest.SuggestStatus.PAY_IN_PROGRESS)) {
-            throw new BusinessLogicException(ExceptionCode.INVALID_ACCESS);
-        }
-
-        suggestRepository.delete(findSuggest);
-    }
-
-    public void declineSuggest(Long lessonId, Long suggestId, Long memberId, PostReason postReason) {
-
-        Suggest findSuggest = ifExistsReturnSuggest(suggestId);
-        Lesson findLesson = lessonDbService.ifExistsReturnLesson(lessonId);
-
-        if (!memberId.equals(findLesson.getMemberId()) ||
-            !findSuggest.getStatus().equals(Suggest.SuggestStatus.ACCEPT_IN_PROGRESS)) {
-            throw new BusinessLogicException(ExceptionCode.INVALID_ACCESS);
-        }
-
-        Reason reason = Reason.builder().reason(postReason.getReason()).build();
-        reasonRepository.save(reason);
-
-        suggestRepository.delete(findSuggest);
-
-    }
 }
--- server/src/main/java/com/codueon/boostUp/domain/suggest/service/SuggestService.java
+++ server/src/main/java/com/codueon/boostUp/domain/suggest/service/SuggestService.java
@@ -2,7 +2,10 @@ package com.codueon.boostUp.domain.suggest.service;
 
 import com.codueon.boostUp.domain.lesson.entity.Lesson;
 import com.codueon.boostUp.domain.lesson.service.LessonDbService;
+import com.codueon.boostUp.domain.suggest.dto.GetSuggestInfo;
+import com.codueon.boostUp.domain.suggest.dto.PostReason;
 import com.codueon.boostUp.domain.suggest.dto.PostSuggest;
+import com.codueon.boostUp.domain.suggest.entity.Reason;
 import com.codueon.boostUp.domain.suggest.entity.Suggest;
 import com.codueon.boostUp.global.exception.BusinessLogicException;
 import com.codueon.boostUp.global.exception.ExceptionCode;
@@ -57,6 +60,52 @@ public class SuggestService {
 
         suggestDbService.saveSuggest(findSuggest);
 
+    }
+
+    public void cancelSuggest(Long suggestId, Long memberId) {
+
+        Suggest findSuggest = suggestDbService.ifExistsReturnSuggest(suggestId);
+
+        if (!memberId.equals(findSuggest.getMemberId())) {
+            throw new BusinessLogicException(ExceptionCode.INVALID_ACCESS);
+        }
+
+        if (!findSuggest.getStatus().equals(Suggest.SuggestStatus.ACCEPT_IN_PROGRESS) &&
+                !findSuggest.getStatus().equals(Suggest.SuggestStatus.PAY_IN_PROGRESS)) {
+            throw new BusinessLogicException(ExceptionCode.INVALID_ACCESS);
+        }
+
+        suggestDbService.deleteSuggest(findSuggest);
+    }
+
+    public void declineSuggest(Long lessonId, Long suggestId, Long memberId, PostReason postReason) {
+
+        Suggest findSuggest = suggestDbService.ifExistsReturnSuggest(suggestId);
+        Lesson findLesson = lessonDbService.ifExistsReturnLesson(lessonId);
+
+        if (!memberId.equals(findLesson.getMemberId()) ||
+                !findSuggest.getStatus().equals(Suggest.SuggestStatus.ACCEPT_IN_PROGRESS)) {
+            throw new BusinessLogicException(ExceptionCode.INVALID_ACCESS);
+        }
+
+        Reason reason = Reason.builder().reason(postReason.getReason()).build();
+        suggestDbService.saveReason(reason);
+
+        suggestDbService.deleteSuggest(findSuggest);
+
+    }
+
+    public GetSuggestInfo getSuggestInfo(Long suggestId, Long lessonId, Long memberId) {
+
+        Lesson findLesson = lessonDbService.ifExistsReturnLesson(lessonId);
+        Suggest findSuggest = suggestDbService.ifExistsReturnSuggest(suggestId);
+
+        if (!memberId.equals(findSuggest.getMemberId()) ||
+            findSuggest.getStatus().equals(Suggest.SuggestStatus.ACCEPT_IN_PROGRESS)) {
+            throw new BusinessLogicException(ExceptionCode.INVALID_ACCESS);
+        }
+
+        return new GetSuggestInfo(findLesson, findSuggest.getTotalCost(), findSuggest.getQuantity());
     }
 
 
