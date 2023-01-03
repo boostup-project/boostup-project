4e54a85385b6ce1c5d501f79373049b3428543e9
seonghun Park <106587166+hun0613@users.noreply.github.com>
2023-01-03 17:14:26 +0900

    feat: 인증페이지 재사용컴포넌트 구현 (#152)

---
+++ client/components/reuse/AuthBtn.tsx
@@ -0,0 +1,16 @@
+interface Props {
+    onClick : () => void;
+    children : React.ReactNode;
+}
+
+const AuthBtn = ({onClick, children}:Props) => {
+    return (
+        <>
+            <button className="font-SCDream4 w-1/4 py-2 bg-pointColor rounded-md text-white text-sm" onClick={onClick}>
+                {children}
+            </button>
+        </>
+    )
+}
+
+export default AuthBtn;
