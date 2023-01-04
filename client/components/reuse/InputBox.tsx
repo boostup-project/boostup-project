859985e5ba75e1a9fd4d0e35a21a08f76e133ea3
headring <myhakiki@gmail.com>
2023-01-04 14:05:38 +0900

    style: placeholder 텍스트 중앙 배치

--- client/components/reuse/InputBox.tsx
+++ client/components/reuse/InputBox.tsx
@@ -1,15 +1,21 @@
 interface Props {
-    placeholder : string,
-    value : string,
-    onChange : (e:any) => void;
+  placeholder: string;
+  value: string;
+  onChange: (e: any) => void;
 }
 
-const InputBox = ({placeholder, value, onChange}:Props) => {
-    return (
-        <>
-            <input type="text" placeholder={placeholder} value={value} onChange={onChange} className="w-full h-fit p-2 border  border-borderColor outline-pointColor rounded-md font-SCDream2 text-sm text-textColor"></input>
-        </>
-    )
-}
+const InputBox = ({ placeholder, value, onChange }: Props) => {
+  return (
+    <>
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="w-full h-fit p-2 border  border-borderColor outline-pointColor rounded-md font-SCDream2 text-sm text-textColor placeholder:text-center"
+      ></input>
+    </>
+  );
+};
 
 export default InputBox;
