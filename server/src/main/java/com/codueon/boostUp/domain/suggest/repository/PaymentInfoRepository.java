c7d11dcd853a423de84c28e4df010d375fe6044f
LeeGoh <gyuleegoh@gmail.com>
2023-01-05 15:15:10 +0900

    feat: 결제 기능, 과외 종료 기능 구현 및 주석 추가

--- server/src/main/java/com/codueon/boostUp/domain/suggest/repository/PaymentInfoRepository.java
+++ server/src/main/java/com/codueon/boostUp/domain/suggest/repository/PaymentInfoRepository.java
@@ -3,5 +3,10 @@ package com.codueon.boostUp.domain.suggest.repository;
 import com.codueon.boostUp.domain.suggest.entity.PaymentInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentInfoRepository extends JpaRepository<PaymentInfo, Long> {
+
+    Optional<PaymentInfo> findBySuggestId(Long suggestId);
+
 }
--- server/src/main/java/com/codueon/boostUp/domain/suggest/service/SuggestService.java
+++ server/src/main/java/com/codueon/boostUp/domain/suggest/service/SuggestService.java
@@ -10,9 +10,7 @@ import com.codueon.boostUp.domain.suggest.dto.PostSuggest;
 import com.codueon.boostUp.domain.suggest.entity.PaymentInfo;
 import com.codueon.boostUp.domain.suggest.entity.Reason;
 import com.codueon.boostUp.domain.suggest.entity.Suggest;
-import com.codueon.boostUp.domain.suggest.pay.KakaoPayHeader;
-import com.codueon.boostUp.domain.suggest.pay.PayReadyInfo;
-import com.codueon.boostUp.domain.suggest.pay.ReadyToPaymentInfo;
+import com.codueon.boostUp.domain.suggest.pay.*;
 import com.codueon.boostUp.domain.suggest.response.Message;
 import com.codueon.boostUp.global.exception.BusinessLogicException;
 import com.codueon.boostUp.global.exception.ExceptionCode;
@@ -21,6 +19,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.codueon.boostUp.domain.suggest.entity.Suggest.SuggestStatus.END_OF_LESSON;
+import static com.codueon.boostUp.domain.suggest.utils.PayConstants.ORDER_APPROVED;
 import static com.codueon.boostUp.domain.suggest.utils.SuggestConstants.*;
 
 @Slf4j
@@ -31,9 +31,15 @@ public class SuggestService {
     private final SuggestDbService suggestDbService;
     private final LessonDbService lessonDbService;
     private final MemberDbService memberDbService;
-
     private final FeignService feignService;
 
+    /**
+     * 신청 생성 메서드
+     * @param post 신청 생성 DTO
+     * @param lessonId 과외 식별자
+     * @param memberId 사용자 식별자
+     * @author LeeGoh
+     */
     @Transactional
     public void createSuggest(PostSuggest post, Long lessonId, Long memberId) {
 
@@ -56,6 +62,13 @@ public class SuggestService {
         suggestDbService.saveSuggest(suggest);
     }
 
+    /**
+     * 신청 수락 메서드
+     * @param suggestId 신청 식별자
+     * @param memberId 사용자 식별자
+     * @param quantity 과외 횟수
+     * @author LeeGoh
+     */
     @Transactional
     public void acceptSuggest(Long suggestId, Long memberId, Integer quantity) {
 
@@ -76,6 +89,12 @@ public class SuggestService {
 
     }
 
+    /**
+     * 신청 취소 메서드
+     * @param suggestId 신청 식별자
+     * @param memberId 사용자 식별자
+     * @author LeeGoh
+     */
     public void cancelSuggest(Long suggestId, Long memberId) {
 
         Suggest findSuggest = suggestDbService.ifExistsReturnSuggest(suggestId);
@@ -92,6 +111,12 @@ public class SuggestService {
         suggestDbService.deleteSuggest(findSuggest);
     }
 
+    /**
+     * 신청 거절 메서드
+     * @param suggestId 신청 식별자
+     * @param memberId 사용자 식별자
+     * @param postReason 거절 사유
+     */
     public void declineSuggest(Long suggestId, Long memberId, PostReason postReason) {
 
         Suggest findSuggest = suggestDbService.ifExistsReturnSuggest(suggestId);
@@ -109,6 +134,12 @@ public class SuggestService {
 
     }
 
+    /**
+     * 결제 페이지 요청 메서드
+     * @param suggestId 신청 식별자
+     * @param memberId 사용자 식별자
+     * @return GetSuggestInfo
+     */
     public GetSuggestInfo getSuggestInfo(Long suggestId, Long memberId) {
 
         Suggest findSuggest = suggestDbService.ifExistsReturnSuggest(suggestId);
@@ -122,6 +153,15 @@ public class SuggestService {
         return new GetSuggestInfo(findLesson, findSuggest.getTotalCost(), findSuggest.getQuantity());
     }
 
+    /**
+     * 결제 URL 요청 메서드
+     * @param suggestId 신청 식별자
+     * @param memberId 사용자 식별자
+     * @param requestUrl 요청 URL
+     * @return Message
+     * @author LeeGoh
+     */
+    @Transactional
     public Message getKaKapPayUrl(Long suggestId, Long memberId, String requestUrl) {
 
         Suggest findSuggest = suggestDbService.ifExistsReturnSuggest(suggestId);
@@ -129,7 +169,7 @@ public class SuggestService {
         Lesson findLesson = lessonDbService.ifExistsReturnLesson(findSuggest.getLessonId());
 
         KakaoPayHeader headers = feignService.setHeaders();
-        ReadyToPaymentInfo params = feignService.setParams(requestUrl, findSuggest, findMember, findLesson);
+        ReadyToPaymentInfo params = feignService.setReadyParams(requestUrl, findSuggest, findMember, findLesson);
 
         PayReadyInfo payReadyInfo = feignService.getPayReadyInfo(headers, params);
 
@@ -148,10 +188,61 @@ public class SuggestService {
 
     }
 
+    /**
+     * URL 요청 시 리턴되는 URL이 없을 때 발생하는 이벤트 메서드
+     * @return Message
+     * @author LeeGoh
+     */
     public Message<?> getFailedPayMessage() {
         return Message.builder()
                 .message(FAILED_INFO_MESSAGE + "<br>" + INVALID_PARAMS)
                 .build();
+    }
+
+    /**
+     * 결제 성공 시 예약 정보 반환 메서드
+     * @param suggestId 신청 식별자
+     * @param pgToken Payment Gateway Token
+     * @return Message
+     * @author LeeGoh
+     */
+    @Transactional
+    public Message getSuccessPaymentInfo(Long suggestId, String pgToken) {
+
+        Suggest findSuggest = suggestDbService.ifExistsReturnSuggest(suggestId);
+        PaymentInfo findPaymentInfo = suggestDbService.ifExistsReturnPaymentInfo(suggestId);
+
+        KakaoPayHeader headers = feignService.setHeaders();
+        RequestForPaymentInfo params = feignService.setRequestParams(pgToken, findPaymentInfo);
+
+        PaySuccessInfo paySuccessInfo = feignService.getSuccessResponse(headers, params);
+
+        paySuccessInfo.setOrderStatus(ORDER_APPROVED);
+        findSuggest.setStatus(Suggest.SuggestStatus.DURING_LESSON);
+        suggestDbService.saveSuggest(findSuggest);
+        suggestDbService.savePayment(findPaymentInfo);
+
+        return Message.builder()
+                .data(paySuccessInfo)
+                .message(INFO_URI_MSG)
+                .build();
+
+    }
+
+    /**
+     * 과외 종료 시 신청 상태 및 종료 시간 저장 메서드
+     * @param suggestId 신청 식별자
+     * @author LeeGoh
+     */
+    public void setSuggestStatusAndEndTime(Long suggestId) {
+
+        Suggest findSuggest = suggestDbService.ifExistsReturnSuggest(suggestId);
+
+        findSuggest.setStatus(END_OF_LESSON);
+        findSuggest.setEndTime();
+
+        suggestDbService.saveSuggest(findSuggest);
+
     }
 
 
