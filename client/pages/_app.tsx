9d2cb0af91ca681f8961ff9ab7141e0335efbc4c
SangHyun Kim <48895268+headring@users.noreply.github.com>
2023-01-06 15:35:33 +0900

    feat: 페이지 조립 (#193)

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
+import Header from "components/Header/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
@@ -10,7 +11,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <QueryClientProvider client={queryClient}>
         <RecoilRoot>
-          <Component {...pageProps} />
+          <div className="flex flex-col justify-center items-center bg-bgColor">
+            <Header />
+            <Component {...pageProps} />
+          </div>
         </RecoilRoot>
       </QueryClientProvider>
     </>
