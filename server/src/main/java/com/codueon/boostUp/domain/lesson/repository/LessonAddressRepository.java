4410306f19e9ea491e32ca71eec39de035631ed6
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-01-09 15:52:38 +0900

    feat: lesson 수정 구현중 (#223)

--- server/src/main/java/com/codueon/boostUp/domain/lesson/repository/LessonAddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codueon.boostUp.domain.lesson.repository;
-
-import com.codueon.boostUp.domain.lesson.entity.LessonAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LessonAddressRepository extends JpaRepository<LessonAddress, Long> {
-}
