22717fe2ac0daf12ad0d2df33dea6e62a275753f
seonghun Park <106587166+hun0613@users.noreply.github.com>
2023-01-11 17:04:16 +0900

    feat: 글작성모달 진행방식 컴포넌트 구현 (#256)
    * style: 진행방식 퍼블리싱 완료
    
    * feat: 진행방식 마크업 input 구현
    
    * Update useWindowSize.tsx

--- client/apis/auth/postAuthPw.tsx
+++ client/apis/auth/postAuthPw.tsx
@@ -1,20 +1,21 @@
 import axios from "axios";
 
 interface Props {
-    name : string,
-    email : string
+  name: string;
+  email: string;
 }
 
-const postAuthPw = async({name, email}:Props) => {
-    const data = {
-        name : name,
-        email : email
-    }
+const postAuthPw = async ({ name, email }: Props) => {
+  const data = {
+    name: name,
+    email: email,
+  };
 
-    console.log(data);
-    return await axios.post(`/auth/find/password`, data, {
-        baseURL: process.env.NEXT_PUBLIC_API_URL,
-    })
-}
+  console.log(data);
+  // return await axios.post(`/auth/find/password`, data, {
+  return await axios.post(`/member/find/password`, data, {
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+  });
+};
 
 export default postAuthPw;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import CreateModalContainer from "components/reuse/container/CreateModalContainer";
-import SmallBtn from "components/reuse/btn/SmallBtn";
+// import SmallBtn from "components/reuse/btn/SmallBtn";
 import StepNavWrapper from "components/reuse/container/StepNavWrapper";
+import Curriculum from "components/createModal/Curriculum";
+
 const Home = () => {
   return (
     <>
       <div className="flex flex-col bg-bgColor items-center justify-center w-full h-screen">
         <CreateModalContainer>
           <StepNavWrapper />
-          <SmallBtn>
-            다음
-          </SmallBtn>
+          <Curriculum />
         </CreateModalContainer>
       </div>
     </>
