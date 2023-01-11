5592da69532a076663d25008140fef6673e2427e
headring <myhakiki@gmail.com>
2023-01-11 15:14:06 +0900

    test: 렌더링 테스트 코드

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import CreateModalContainer from "components/reuse/container/CreateModalContainer";
 import SmallBtn from "components/reuse/btn/SmallBtn";
 import StepNavWrapper from "components/reuse/container/StepNavWrapper";
+import Additional from "components/Additional";
+
 const Home = () => {
   return (
     <>
       <div className="flex flex-col bg-bgColor items-center justify-center w-full h-screen">
         <CreateModalContainer>
           <StepNavWrapper />
-          <SmallBtn>
-            다음
-          </SmallBtn>
+          <Additional />
+          <SmallBtn>다음</SmallBtn>
         </CreateModalContainer>
       </div>
     </>
