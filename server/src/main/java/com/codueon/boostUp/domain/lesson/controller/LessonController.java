8aeee052802bdf6c47a80ccb6a1492b409d57fcd
quartz614 <quartz614@naver.com>
2023-01-13 17:43:53 +0900

    feat: 마이페이지 등록한 과외 조회

--- server/src/main/java/com/codueon/boostUp/domain/lesson/controller/LessonController.java
+++ server/src/main/java/com/codueon/boostUp/domain/lesson/controller/LessonController.java
@@ -1,12 +1,12 @@
 package com.codueon.boostUp.domain.lesson.controller;
 
-import com.codueon.boostUp.domain.lesson.dto.PatchLessonCurriculum;
-import com.codueon.boostUp.domain.lesson.dto.PostLesson;
-import com.codueon.boostUp.domain.lesson.dto.PostLessonDetailEdit;
-import com.codueon.boostUp.domain.lesson.dto.PostLessonInfoEdit;
+import com.codueon.boostUp.domain.dto.MultiResponseDto;
+import com.codueon.boostUp.domain.lesson.dto.*;
 import com.codueon.boostUp.domain.lesson.service.LessonService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/lesson")
 public class LessonController {
     private final LessonService lessonService;
 
@@ -28,16 +29,16 @@ public class LessonController {
      * @throws Exception
      * @author Quartz614
      */
-    @PostMapping(value = "/lesson/registration")
+    @PostMapping(value = "/registration")
     public ResponseEntity<?> postLesson(@RequestPart(value = "data") PostLesson postLesson,
                                         @RequestPart(value = "profileImage") MultipartFile profileImage,
                                         @RequestPart(value = "careerImage") List<MultipartFile> careerImage) throws Exception {
         Long memberId = 1L;
         /** 로컬 환경 */
-//        lessonService.createLesson(postLesson, memberId, profileImage, careerImage);
+        lessonService.createLesson(postLesson, memberId, profileImage, careerImage);
 
         /** S3 환경 */
-        lessonService.createLessonS3(postLesson, memberId, profileImage, careerImage);
+//        lessonService.createLessonS3(postLesson, memberId, profileImage, careerImage);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
@@ -50,16 +51,16 @@ public class LessonController {
      * @author Quatz614
      */
     @SneakyThrows
-    @PostMapping("/lesson/{lesson-id}/modification")
+    @PostMapping("/{lesson-id}/modification")
     public ResponseEntity updateLesson(@PathVariable("lesson-id") Long lessonId,
                                        @RequestPart(value = "data") PostLessonInfoEdit postLessonInfoEdit,
                                        @RequestPart(value = "profileImage") MultipartFile profileImage) {
         Long memberId = 1L;
         /** 로컬 환경 */
-//        lessonService.updateLessonInfo(lessonId, postLessonInfoEdit, memberId, profileImage);
+        lessonService.updateLessonInfo(lessonId, postLessonInfoEdit, memberId, profileImage);
 
         /** S3 환경 */
-        lessonService.updateLessonInfoS3(lessonId, postLessonInfoEdit, memberId, profileImage);
+//        lessonService.updateLessonInfoS3(lessonId, postLessonInfoEdit, memberId, profileImage);
         return ResponseEntity.ok().build();
     }
 
@@ -72,16 +73,16 @@ public class LessonController {
      * @author Quartz614
      */
     @SneakyThrows
-    @PostMapping("/lesson/{lesson-id}/detailInfo/modification")
+    @PostMapping("/{lesson-id}/detailInfo/modification")
     public ResponseEntity updateLessonDetail(@PathVariable("lesson-id") Long lessonId,
                                              @RequestPart(value = "data") PostLessonDetailEdit postLessonDetailEdit,
                                              @RequestPart(value = "careerImage") List<MultipartFile> careerImage) {
         Long memberId = 1L;
         /** 로컬 환경 */
-//        lessonService.updateLessonDetail(lessonId, postLessonDetailEdit, memberId, careerImage);
+        lessonService.updateLessonDetail(lessonId, postLessonDetailEdit, memberId, careerImage);
 
         /** S3 환경 */
-        lessonService.updateLessonDetailS3(lessonId, postLessonDetailEdit, memberId, careerImage);
+//        lessonService.updateLessonDetailS3(lessonId, postLessonDetailEdit, memberId, careerImage);
 
         return ResponseEntity.ok().build();
     }
@@ -93,7 +94,7 @@ public class LessonController {
      * @return ResponseEntity
      * @author Quartz614
      */
-    @PatchMapping("/lesson/{lesson-id}/curriculum/modification")
+    @PatchMapping("/{lesson-id}/curriculum/modification")
     public ResponseEntity updateCurriculum(@PathVariable("lesson-id") Long lessonId,
                                            @RequestBody PatchLessonCurriculum patchLessonCurriculum) {
         Long memberId = 1L;
@@ -108,15 +109,109 @@ public class LessonController {
      * @return ResponseEntity
      * @author Quartz614
      */
-    @DeleteMapping(value = "/lesson/{lesson-id}")
+    @DeleteMapping(value = "/{lesson-id}")
     public ResponseEntity deleteLesson(@PathVariable("lesson-id") Long lessonId) {
         Long memberId = 1L;
         /** 로컬 환경 */
-//        lessonService.deleteLesson(memberId, lessonId);
+        lessonService.deleteLesson(memberId, lessonId);
 
         /** S3 환경 */
-        lessonService.deleteLessonS3(memberId, lessonId);
+//        lessonService.deleteLessonS3(memberId, lessonId);
 
         return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * 등록한 과외 마이페이지 조회 컨트롤러
+     * @return ResponseEntity
+     * @author Quartz614
+     */
+    @GetMapping(value = "/tutor")
+    public ResponseEntity<String> getLessonMypage() {
+        Long memberId = 1L;
+        String response = lessonService.getLessonMypage(memberId);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    /**
+     * 메인페이지 과외 전체 조회 컨트롤러 메서드
+     * @param pageable 페이지 정보
+     * @return Page(GetMainPageLesson)
+     * @author mozzi327
+     */
+    @GetMapping
+    public ResponseEntity<?> getMainPageLessonInfos(Pageable pageable) {
+        // TODO : 시큐리티 적용 시 Authentication 객체 추가 요
+        Long memberId = 1L;
+        Page<GetMainPageLesson> response = lessonService.getMainPageLessons(memberId, pageable);
+        return ResponseEntity.ok().body(new MultiResponseDto<>(response));
+    }
+    /**
+     * 메인페이지 상세 검색 컨트롤러 메서드
+     * @param postSearchLesson 상세 검색 정보
+     * @param pageable 페이지 정보
+     * @return Page(GetMainPageLesson)
+     * @author mozzi327
+     */
+    @PostMapping("/search")
+    public ResponseEntity<?> getDetailSearchForLesson(@RequestBody PostSearchLesson postSearchLesson,
+                                                      Pageable pageable) {
+        // TODO : 시큐리티 적용 시 Authentication 객체 추가 요
+        Long memberId = 1L;
+        Page<GetMainPageLesson> response = lessonService.getDetailSearchLessons(memberId, postSearchLesson, pageable);
+        return ResponseEntity.ok().body(new MultiResponseDto<>(response));
+    }
+
+    /**
+     * 메인페이지 언어 별 과외 조회 컨트롤러 메서드
+     * @param languageId 사용 언어 식별자
+     * @param pageable 페이지 정보
+     * @return Page(GetMainPageLesson)
+     * @author mozzi327
+     */
+    @GetMapping("/language/{language-id}")
+    public ResponseEntity<?> getLessonByLanguage(@PathVariable("language-id") Long languageId,
+                                                 Pageable pageable) {
+        // TODO : 시큐리티 적용 시 Authentication 객체 추가 요
+        Long memberId = 1L;
+        Page<GetMainPageLesson> response = lessonService.getMainPageLessonsAboutLanguage(memberId, languageId, pageable);
+        return ResponseEntity.ok().body(new MultiResponseDto<>(response));
+    }
+
+    /**
+     * 과외 상세 페이지 요약 정보 조회 컨트롤러 메서드
+     * @param lessonId 과외 식별자
+     * @return GetLesson
+     * @author mozzi327
+     */
+    @GetMapping("/{lesson-id}")
+    public ResponseEntity<?> getLesson(@PathVariable("lesson-id") Long lessonId) {
+        GetLesson response = lessonService.getDetailLesson(lessonId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    /**
+     * 과외 상세 페이지 상세 정보 조회 컨트롤러 메서드
+     * @param lessonId
+     * @return GetLessonInfo
+     * @author mozzi327
+     */
+    @GetMapping("/{lesson-id}/detailInfo")
+    public ResponseEntity<?> getLessonInfo(@PathVariable("lesson-id") Long lessonId) {
+        GetLessonInfo response = lessonService.getDetailLessonInfo(lessonId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    /**
+     * 과외 상세 페이지 커리큘럼 정보 조회 컨트롤러 메서드
+     * @param lessonId
+     * @return GetLessonCurriculum
+     * @author mozzi327
+     */
+    @GetMapping("/{lesson-id}/curriculum")
+    public ResponseEntity<?> getCurriculum(@PathVariable("lesson-id") Long lessonId) {
+        GetLessonCurriculum response = lessonService.getDetailLessonCurriculum(lessonId);
+        return ResponseEntity.ok().body(response);
     }
 }
