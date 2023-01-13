cdc1fba9f6a0137a1bb912f40c4a69997e7b7af2
headring <myhakiki@gmail.com>
2023-01-13 15:49:07 +0900

    feat:NavBar파일 생성

---
+++ client/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div className="bg-white flex-1 fixed left-0 right-0 bottom-0">
+      <div className="flex">
+        <div>home</div>
+        <div>mypage</div>
+        <div>write</div>
+        <div>message</div>
+        <div>langFilter</div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
