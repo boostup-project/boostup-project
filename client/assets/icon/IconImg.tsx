081420eda3aef6521ca9b06cebb0adff2e5cbf58
headring <myhakiki@gmail.com>
2023-01-13 17:34:30 +0900

    refactor: SVG 파일 fill부분 수정

--- client/assets/icon/IconImg.tsx
+++ client/assets/icon/IconImg.tsx
@@ -15,7 +15,7 @@ export const IconImg = ({ width, heigth, fill }: Props) => {
     >
       <path
         d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
-        fill={fill ? fill : "currentValue"}
+        fill={fill ? fill : "black"}
       />
     </svg>
   );
