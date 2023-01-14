317e078f800bc3da7e45506e8a67770b466ed0b4
headring <myhakiki@gmail.com>
2023-01-14 11:36:14 +0900

    feat: Navbar 작성중 - 아이콘 삽입

--- client/components/Navbar/Navbar.tsx
+++ client/components/Navbar/Navbar.tsx
@@ -1,13 +1,54 @@
+import {
+  IconChat,
+  IconCode,
+  IconHome,
+  IconProfile,
+  IconPlus,
+} from "assets/icon/";
+import Link from "next/link";
+
+const navContents = [
+  {
+    type: "Link",
+    image: <IconHome />,
+    link: "/",
+  },
+  {
+    type: "Link",
+    image: <IconProfile />,
+    link: "/",
+  },
+  {
+    type: "div",
+    image: <IconPlus />,
+  },
+  {
+    type: "Link",
+    image: <IconChat />,
+    link: "/",
+  },
+  {
+    type: "div",
+    image: <IconCode />,
+  },
+];
+
 const Navbar = () => {
   return (
-    <div className="bg-white flex-1 fixed left-0 right-0 bottom-0">
-      <div className="flex">
-        <div>home</div>
-        <div>mypage</div>
-        <div>write</div>
-        <div>message</div>
-        <div>langFilter</div>
-      </div>
+    <div className="bg-white w-full h-16 flex justify-center items-center sticky bottom-0  desktop:hidden">
+      {navContents.map((content, i) => (
+        <div className="w-1/5 flex justify-center items-center">
+          {content.type === "Link" ? (
+            <div className="w-6">
+              <Link href={content.link!}>{content.image}</Link>
+            </div>
+          ) : (
+            <div className="w-6">
+              <div className="bg-pointColor">{content.image}</div>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
