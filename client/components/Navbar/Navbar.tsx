636c76c5a3d0f9828e38d1b7159eb010cdb69264
headring <myhakiki@gmail.com>
2023-01-16 17:01:10 +0900

    refactor: key값을 map return문의 최상위에 배치

--- client/components/Navbar/Navbar.tsx
+++ client/components/Navbar/Navbar.tsx
@@ -37,20 +37,17 @@ const Navbar = () => {
   return (
     <div className="bg-white w-full h-16 flex justify-center items-center sticky bottom-0  desktop:hidden">
       {navContents.map((content, i) => (
-        <div className="w-1/5 flex justify-center items-center">
+        <div key={i} className="w-1/5 flex justify-center items-center">
           {content.type === "Link" ? (
-            <div key={i} className="w-7">
+            <div className="w-7">
               <Link href={content.link!}>{content.image}</Link>
             </div>
           ) : content.type === "Write" ? (
-            <div
-              key={i}
-              className="w-12 h-12 rounded-3xl bg-pointColor flex justify-center items-center"
-            >
+            <div className="w-12 h-12 rounded-3xl bg-pointColor flex justify-center items-center">
               <div className="w-5">{content.image}</div>
             </div>
           ) : (
-            <div key={i} className="w-7">
+            <div className="w-7">
               <div>{content.image}</div>
             </div>
           )}
