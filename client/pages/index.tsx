33d25a1774b2808213487612d84b6d27c88f1b14
headring <myhakiki@gmail.com>
2023-01-16 15:06:42 +0900

    refactor: import문 수정

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import CreateModalContainer from "components/reuse/container/CreateModalContainer";
 // import SmallBtn from "components/reuse/btn/SmallBtn";
 import StepNavWrapper from "components/reuse/container/StepNavWrapper";
-import Additional from "components/Additional";
-import Curriculum from "components/createModal/Curriculum";
 import BasicInfo from "components/createModal/BasicInfo";
+import Additional from "components/createModal/Additional";
+import Curriculum from "components/createModal/Curriculum";
 import Carouselcomp from "components/carousel/Carouselcomp";
 
 const Home = () => {
