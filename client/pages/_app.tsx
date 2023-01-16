350b3162b76048b1e468c4e71af53e0b24fefdd7
headring <myhakiki@gmail.com>
2023-01-16 15:05:02 +0900

    feat: 모바일 Navbar 장착

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
 import Header from "components/Header/Header";
 import Footer from "components/Footer/Footer";
+import Navbar from "components/Navbar/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
             <Header />
             <Component {...pageProps} />
             <Footer />
+            <Navbar />
           </div>
         </RecoilRoot>
       </QueryClientProvider>
