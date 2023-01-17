cee60aa843f311f832a04388b6d9ddc4c9fdfd25
LimJaeminZ <107789417+LimJaeminZ@users.noreply.github.com>
2023-01-17 21:22:31 +0900

    feat : 로그아웃 기능 구현 (#322)

--- server/src/main/java/com/codueon/boostUp/domain/member/service/AuthService.java
+++ server/src/main/java/com/codueon/boostUp/domain/member/service/AuthService.java
@@ -4,6 +4,8 @@ import com.codueon.boostUp.domain.member.dto.AuthDto;
 import com.codueon.boostUp.domain.member.dto.PostLogin;
 import com.codueon.boostUp.domain.member.dto.TokenDto;
 import com.codueon.boostUp.domain.member.entity.Member;
+import com.codueon.boostUp.domain.member.exception.AuthException;
+import com.codueon.boostUp.global.exception.ExceptionCode;
 import com.codueon.boostUp.global.security.utils.JwtTokenUtils;
 import com.codueon.boostUp.global.utils.RedisUtils;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +25,12 @@ public class AuthService {
     private final JwtTokenUtils jwtTokenUtils;
     private final RedisUtils redisUtils;
 
+    /**
+     * 사용자 로그인
+     * @param login 로그인 정보
+     * @return
+     * @author LimJaeminZ
+     */
     public TokenDto.Response loginMember(PostLogin login) {
         Member findMember = memberDbService.ifExistsMemberByEmail(login.getEmail());
 
@@ -36,7 +44,7 @@ public class AuthService {
         headers.add(REFRESH_TOKEN, generateRefreshToken);
 
         //redis 저장
-        redisUtils.setData(generateRefreshToken, findMember.getId(), jwtTokenUtils.getRefreshTokenExpirationMinutes());
+        redisUtils.setData(findMember.getEmail(), findMember.getAccountStatus().getProvider(), generateRefreshToken, jwtTokenUtils.getRefreshTokenExpirationMinutes());
 
         AuthDto memberRes = AuthDto.builder()
                 .email(findMember.getEmail())
@@ -48,4 +56,30 @@ public class AuthService {
                 .headers(headers)
                 .build();
     }
+
+    /**
+     * 사용자 로그아웃
+     * @param accessToken
+     * @param refreshToken
+     * @param memberId
+     * @author LimJaeminZ
+     */
+    public void logoutMember(String accessToken, String refreshToken, Long memberId) {
+        Member findMember = memberDbService.ifExistsReturnMember(memberId);
+
+        accessToken = jwtTokenUtils.parseAccessToken(accessToken);
+
+        // accessToken 검증
+        if (!jwtTokenUtils.validateToken(accessToken)) {
+           throw new AuthException(ExceptionCode.INVALID_AUTH_TOKEN);
+        }
+
+        // refreshToken 존재 시 삭제
+        redisUtils.deleteData(findMember.getEmail(), findMember.getAccountStatus().getProvider());
+
+        // accessToken 만료 전까지 블랙리스트 처리
+        Long expiration = jwtTokenUtils.getExpiration(accessToken);
+        redisUtils.setBlackList(accessToken, "Logout", expiration);
+    }
 }
+
