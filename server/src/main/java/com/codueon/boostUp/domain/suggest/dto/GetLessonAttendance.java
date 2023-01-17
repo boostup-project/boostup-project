a2ca1f66add9be09008160c2810ebd2d3616aaa3
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2023-01-17 15:09:04 +0900

    refactor: 출석부 조회 시 Response 데이터 변경 (#314)
    Co-authored-by: LeeGoh <“gyuleegoh@gmail.com”>

---
+++ server/src/main/java/com/codueon/boostUp/domain/suggest/dto/GetLessonAttendance.java
@@ -0,0 +1,25 @@
+package com.codueon.boostUp.domain.suggest.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GetLessonAttendance {
+
+    private Integer quantity;
+
+    private Integer quantityCount;
+
+    private Integer progress;
+
+    @Builder
+    public GetLessonAttendance(Integer quantity,
+                               Integer quantityCount,
+                               Integer progress) {
+        this.quantity = quantity;
+        this.quantityCount = quantityCount;
+        this.progress = progress;
+    }
+}
