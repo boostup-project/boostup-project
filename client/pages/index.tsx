297661b9879af5870a6d070f86a0d6a6f7f5c2f0
headring <myhakiki@gmail.com>
2023-01-18 14:59:31 +0900

    refactor: Modal 부분 삭제

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-import CreateModalContainer from "components/reuse/container/CreateModalContainer";
-// import SmallBtn from "components/reuse/btn/SmallBtn";
-import StepNavWrapper from "components/reuse/container/StepNavWrapper";
-import BasicInfo from "components/createModal/BasicInfo";
-import Additional from "components/createModal/Additional";
-import Curriculum from "components/createModal/Curriculum";
 import Carouselcomp from "components/carousel/Carouselcomp";
 
 const Home = () => {
@@ -12,12 +6,6 @@ const Home = () => {
     <>
       <div className="flex flex-col bg-bgColor items-center justify-start w-full h-screen">
         <Carouselcomp />
-        <CreateModalContainer>
-          <StepNavWrapper />
-          <Additional />
-          <BasicInfo />
-          <Curriculum />
-        </CreateModalContainer>
       </div>
     </>
   );
