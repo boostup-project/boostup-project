0d4fe8c0b06ec938b40f808f814def2290806029
seonghun Park <106587166+hun0613@users.noreply.github.com>
2023-01-18 17:39:12 +0900

    feat: 언어필터기능 api연결 구현 (#332)

---
+++ client/hooks/board/useGetFilteredBoard.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from "react-query";
+import getFilteredBoard from "apis/board/getFilteredBoard";
+
+const useGetFilteredBoard = (languageId: number) => {
+  return useQuery(["get/FilteredBoard"], () => getFilteredBoard(languageId), {
+    enabled: false,
+    onSuccess: res => {
+      console.log(res);
+    },
+    onError: error => {
+      console.log(error);
+    },
+    retry: 2,
+  });
+};
+
+export default useGetFilteredBoard;
