c6abe8a1c41793477612508032392c0a405b7fd4
skynolimit <sky221b9@gmail.com>
2023-01-18 20:59:18 +0900

    styleMainCardEdit

--- client/components/Card/card.tsx
+++ client/components/Card/card.tsx
@@ -2,23 +2,23 @@ import { IconPaper, IconPlace, IconRibbon, IconWon } from "assets/icon/";
 
 const Card = () => {
   return (
-    <div className="h-fit m-1 desktop:w-1/5 tablet:w-1/4 w-1/3 ">
+    <div className="h-fit m-1 desktop:w-1/5 tablet:w-1/4 w-5/12 ">
       <div className="flex flex-col w-full h-1/4 border border-borderColor rounded-lg">
-        <div className="flex w-full desktop:h-full tablet:h-1/3 h-1/2">
+        <div className="flex w-full h-full">
           <img
-            className="rounded-t-lg"
+            className="w-full object-cover rounded-t-lg desktop:h-44 tablet:h-40 h-36"
             src="https://www.medidata.com/wp-content/uploads/2021/11/Medidata_RaveCoder_web_keyfeatures_A1-1.jpg"
           />
         </div>
         <div className="flex flex-col w-full h-2/3">
-          <div className="flex flex-row">
-            <div className="flex justify-center items-center px-1 py-0.5 ml-1 my-1 border rounded-xl bg-[#0A83F2] desktop:text-xs tablet:text-[10px] text-[8px]">
+          <div className="flex flex-row whitespace-wrap">
+            <div className="flex justify-center items-center px-1 py-0.5 ml-1 my-1 border rounded-xl bg-[#0A83F2] desktop:text-xs tablet:text-[10px] text-[6px]">
               JavaScript
             </div>
-            <div className="flex justify-center items-center px-1 py-0.5 ml-1 my-1 border rounded-xl bg-[#6ED5F6] desktop:text-xs tablet:text-[10px] text-[8px]">
+            <div className="flex justify-center items-center px-1 py-0.5 ml-1 my-1 border rounded-xl bg-[#6ED5F6] desktop:text-xs tablet:text-[10px] text-[6px]">
               python
             </div>
-            <div className="flex justify-center items-center px-1 py-0.5 ml-1 my-1 border rounded-xl bg-[#CAD1FA] desktop:text-xs tablet:text-[10px] text-[8px]">
+            <div className="flex justify-center items-center px-1 py-0.5 ml-1 my-1 border rounded-xl bg-[#CAD1FA] desktop:text-xs tablet:text-[10px] text-[6px]">
               kotlin
             </div>
           </div>
@@ -47,7 +47,7 @@ const Card = () => {
             종로구
           </div>
           <div className="flex justify-center items-start w-full h-fit font-SCDream7 desktop:text-base tablet:text-sm text-[12px] text-textColor ml-1 mb-2">
-            <div className="desktop:mt-0.5 tablet:mt-0.5  desktop:w-4 tablet:w-3.5 w-3">
+            <div className="desktop:mt-0.5 tablet:mt-0.5 desktop:w-4 tablet:w-3.5 w-3">
               <IconWon />
             </div>
             <div className="ml-1">65,000/회</div>
--- client/pages/card.tsx
+++ client/pages/card.tsx
@@ -1,8 +1,10 @@
 import Card from "components/Card/card";
-
+//https://8a91-182-226-233-7.jp.ngrok.io
+//Mobile = 375 * 667
 const card = () => {
   return (
-    <div className="mt-32 flex flex-row justify-center  w-full h-[600px] flex-wrap">
+    <div className="mt-24 flex flex-row justify-center flex-wrap desktop:w-10/12 tablet:w-full h-fit">
+      <Card></Card>
       <Card></Card>
       <Card></Card>
       <Card></Card>
