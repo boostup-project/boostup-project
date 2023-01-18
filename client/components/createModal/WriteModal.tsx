3dfd6703466a2d774eea3d3b32ae1e01377ff9e4
headring <myhakiki@gmail.com>
2023-01-18 15:20:16 +0900

    refactor: 모달창 나올 시 현재 위치 고정 및 우측 스크롤바 사라짐

--- client/components/createModal/WriteModal.tsx
+++ client/components/createModal/WriteModal.tsx
@@ -15,13 +15,10 @@ const WriteModal = () => {
   };
   useEffect(() => {
     document.body.style.cssText = `
-    position: fixed; 
-    overflow-y: hidden;
+    overflow: hidden;
     width: 100%;`;
     return () => {
-      const scrollY = document.body.style.top;
-      document.body.style.cssText = "";
-      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+      document.body.style.cssText = "overflow: unset;";
     };
   }, []);
   return (
