8fa89467dc9d2af9ffc095b7b25fe0c546dddaad
headring <myhakiki@gmail.com>
2023-01-19 02:07:48 +0900

    refactor: react-hook-form만으로 제출 성공

--- client/components/createModal/BasicInfo.tsx
+++ client/components/createModal/BasicInfo.tsx
@@ -10,13 +10,6 @@ import { baseSave, inputStep, isWriteModal } from "atoms/main/mainAtom";
 
 interface BasicInfo {
   [index: string]: string | string[] | number;
-  // profileImage: string;
-  // title: string;
-  // language: string[];
-  // company: string;
-  // career: string;
-  // address: string[];
-  // cost: number;
 }
 const BasicInfo = () => {
   const [previewImg, setPreviewImg] = useState<string>("");
@@ -25,14 +18,16 @@ const BasicInfo = () => {
   const [step, setStep] = useRecoilState(inputStep);
   const [base, setBase] = useRecoilState<BasicInfo>(baseSave);
   const setIsWrite = useSetRecoilState(isWriteModal);
-  const [optionErr, setOptionErr] = useState({
-    imgErr: "",
-    checkErr: "",
-    selectErr: "",
-  });
 
   const langArr = Object.keys(langDict);
   const addArr = Object.keys(addDict);
+
+  const options: any = [
+    ...addArr.map((el, idx) => ({
+      value: addDict[el],
+      label: el,
+    })),
+  ];
 
   const {
     control,
@@ -80,36 +75,9 @@ const BasicInfo = () => {
 
   // 넘어가기 전에 잡자
   const onSubmit = (e: BasicInfo) => {
-    if (
-      previewImg.length > 0 &&
-      checkedList.length > 0 &&
-      selectedOptions.length > 0
-    ) {
-      const { title, company, career, cost } = e;
-      const baseSave = {
-        profileImage: previewImg,
-        title,
-        language: checkedList,
-        company,
-        career,
-        address: selectedOptions,
-        cost,
-      };
-      console.log(baseSave);
-      setBase(baseSave);
-      setStep(step + 1);
-    }
-    if (previewImg.length === 0) {
-      setOptionErr(prev => ({ ...prev, previewImg: "필수 정보입니다" }));
-    }
-    if (checkedList.length === 0) {
-      setOptionErr(prev => ({ ...prev, checkErr: "필수 정보입니다" }));
-    }
-    if (selectedOptions.length === 0) {
-      setOptionErr(prev => ({ ...prev, selectErr: "필수 정보입니다" }));
-    }
+    console.log(e);
   };
-  const toWrite = (e: React.MouseEvent<HTMLDivElement>) => {
+  const toWrite = () => {
     setIsWrite(prev => !prev);
   };
 
@@ -159,17 +127,15 @@ const BasicInfo = () => {
               type="file"
               className="opacity-0 rounded-xl"
               accept="image/jpeg,.txt"
+              {...register("profile", { required: "필수 정보입니다" })}
               onChange={e => insertImg(e)}
               defaultValue={base.profileImage}
             />
           </label>
         </div>
-        {previewImg.length === 0 && (
-          <p className="w-11/12 text-xs text-negativeMessage mt-1 tablet:text-sm desktop:w-4/6">
-            에러입니다
-          </p>
-        )}
-
+        <p className="w-11/12 text-xs text-negativeMessage mt-1 tablet:text-sm desktop:w-4/6">
+          {errors?.profile?.message}
+        </p>
         <div className="w-11/12 flex flex-row justify-start items-start h-fit font-SCDream5 text-sm text-textColor mt-4 mb-2 desktop:w-4/6">
           타이틀 <div className="text-pointColor">*</div>
         </div>
@@ -188,48 +154,45 @@ const BasicInfo = () => {
           <div className="text-pointColor">*</div>{" "}
           <div className="text-pointColor text-xs">3개까지 선택가능합니다</div>
         </div>
-        <>
-          <div className="w-11/12 list h-fit flex flex-row items-start desktop:w-4/6">
-            {langArr.map((el: string, idx: number) => {
-              if (idx < 4) {
-                return (
-                  <div key={idx} className="w-1/4 h-fit text-xs">
-                    <label className="checkboxLabel">
-                      <input
-                        type="checkbox"
-                        value={el}
-                        onChange={e => {
-                          onCheckedElement(e.target.checked, e.target.value, e);
-                        }}
-                      />
-                      {el}
-                    </label>
-                  </div>
-                );
-              }
-            })}
-          </div>
-          <div className="w-11/12 list h-fit flex flex-row items-start mt-3 desktop:w-4/6">
-            {langArr.map((el: string, idx: number) => {
-              if (idx > 3) {
-                return (
-                  <div key={idx} className="w-1/4 h-fit text-xs">
-                    <label className="checkboxLabel">
-                      <input
-                        type="checkbox"
-                        value={el}
-                        onChange={e => {
-                          onCheckedElement(e.target.checked, e.target.value, e);
-                        }}
-                      />
-                      {el}
-                    </label>
-                  </div>
-                );
-              }
-            })}
-          </div>
-        </>
+        <div className="w-11/12 list h-fit flex flex-row items-start desktop:w-4/6">
+          {langArr.map((el: string, idx: number) => {
+            if (idx < 4) {
+              return (
+                <div key={idx} className="w-1/4 h-fit text-xs">
+                  <label className="checkboxLabel">
+                    <input
+                      type="checkbox"
+                      value={el}
+                      {...register("language", { required: "true" })}
+                    />
+                    {el}
+                  </label>
+                </div>
+              );
+            }
+          })}
+        </div>
+        <div className="w-11/12 list h-fit flex flex-row items-start mt-3 desktop:w-4/6">
+          {langArr.map((el: string, idx: number) => {
+            if (idx > 3) {
+              return (
+                <div key={idx} className="w-1/4 h-fit text-xs">
+                  <label className="checkboxLabel">
+                    <input
+                      type="checkbox"
+                      value={el}
+                      {...register("language", { required: "true" })}
+                    />
+                    {el}
+                  </label>
+                </div>
+              );
+            }
+          })}
+        </div>
+        <p className="w-11/12 text-xs text-negativeMessage mt-1 tablet:text-sm desktop:w-4/6">
+          {errors?.language && <span>필수 정보입니다</span>}
+        </p>
         <div className="flex flex-row justify-start items-start w-11/12 h-fit font-SCDream5 text-sm text-textColor mt-4 mb-2 desktop:w-4/6">
           재직 회사/학교 <div className="text-pointColor">*</div>
         </div>
@@ -243,7 +206,6 @@ const BasicInfo = () => {
         <p className="w-11/12 text-xs text-negativeMessage mt-1 tablet:text-sm desktop:w-4/6">
           {errors?.company?.message}
         </p>
-
         <div className="flex flex-row justify-start items-start w-11/12 h-fit font-SCDream5 text-sm text-textColor mt-4 mb-2 desktop:w-4/6">
           경력 <div className="text-pointColor">*</div>
         </div>
@@ -259,7 +221,6 @@ const BasicInfo = () => {
         <p className="w-11/12 text-xs text-negativeMessage mt-1 tablet:text-sm desktop:w-4/6">
           {errors?.career?.message}
         </p>
-
         <label className="w-11/12 flex flex-col items-start mt-5 desktop:w-4/6">
           <div className="flex flex-row justify-start items-stretch w-full h-fit font-SCDream5 text-sm text-textColor my-2">
             <div>과외가능지역</div>
@@ -277,15 +238,23 @@ const BasicInfo = () => {
             </div>
             <div className="w-8/12 text-center flex flex-col items-stretch ml-4">
               <div>구</div>
-              <div className="w-full h-fit items-stretch rounded-xl mt-5">
+              <div className="w-full h-fit rounded-xl mt-5">
                 <Controller
-                  name="language"
+                  name="address"
+                  control={control}
+                  rules={{
+                    required: "필수 정보입니다.",
+                    maxLength: {
+                      value: 3,
+                      message: "3개까지만 넣어주세요",
+                    },
+                  }}
                   render={({ field }) => (
                     <Select
                       {...field}
                       placeholder={<div>구 선택</div>}
                       styles={{
-                        control: (baseStyles, state) => ({
+                        control: baseStyles => ({
                           ...baseStyles,
                           borderRadius: "12px",
                           fontSize: "14px",
@@ -300,25 +269,17 @@ const BasicInfo = () => {
                         }),
                       }}
                       isMulti
-                      options={[
-                        ...addArr.map((el, idx) => ({
-                          value: addDict[el].toString(),
-                          label: el.toString(),
-                          key: idx.toString(),
-                        })),
-                      ]}
-                      value={selectedOptions}
-                      onChange={handleAddress}
+                      options={options}
                     />
                   )}
-                  control={control}
                 />
               </div>
             </div>
-            <div className="dropdown-container"></div>
           </div>
         </label>
-
+        <p className="text-right w-11/12 text-xs text-negativeMessage mt-1 tablet:text-sm desktop:w-4/6">
+          {errors?.address?.message}
+        </p>
         <div className="w-11/12 h-fit flex flex-row justify-start items-start font-SCDream5 text-sm text-textColor mt-4 mb-2 desktop:w-4/6">
           수업료<div className="text-pointColor">*</div>
         </div>
@@ -333,7 +294,7 @@ const BasicInfo = () => {
           {errors?.cost?.message}
         </p>
         <div className="flex flex-row justify-center items-center w-full h-fit mt-10">
-          <SmallBtn onClick={e => toWrite(e)}>취 소</SmallBtn>
+          <SmallBtn onClick={toWrite}>취 소</SmallBtn>
           <SmallBtn css="ml-5">다 음</SmallBtn>
         </div>
       </form>
