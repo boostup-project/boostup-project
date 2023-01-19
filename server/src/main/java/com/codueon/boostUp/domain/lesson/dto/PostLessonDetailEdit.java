7fe5fd3340868e9694aaaa691272e00684c5923b
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2023-01-19 23:29:49 +0900

    refactor: lesson controller Authentication token 적용 (#349)
    * refactor: Lesson token 리펙터링
    
    * refactor: Lesson token 리펙터링

--- server/src/main/java/com/codueon/boostUp/domain/lesson/dto/PostLessonDetailEdit.java
+++ server/src/main/java/com/codueon/boostUp/domain/lesson/dto/PostLessonDetailEdit.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 public class PostLessonDetailEdit {
@@ -13,16 +15,20 @@ public class PostLessonDetailEdit {
     private String personality;
     private String detailLocation;
 
+    private List<Long> careerImage;
+
     @Builder
     public PostLessonDetailEdit(String introduction,
                                 String detailCompany,
                                 String detailCost,
                                 String personality,
-                                String detailLocation) {
+                                String detailLocation,
+                                List<Long> careerImage) {
         this.introduction = introduction;
         this.detailCompany = detailCompany;
         this.detailCost = detailCost;
         this.personality = personality;
         this.detailLocation = detailLocation;
+        this.careerImage = careerImage;
     }
 }
