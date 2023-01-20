b98ac788be8057d1d1ec6eed040ac6ccebc37df8
skynolimit <sky221b9@gmail.com>
2023-01-20 16:10:44 +0900

    feat:MainCardQueryEdit

--- client/pages/card.tsx
+++ client/pages/card.tsx
@@ -1,17 +1,9 @@
 import Card from "components/Card/card";
 
-//Mobile = 375 * 667
 const Cards = () => {
   return (
     <div className="mt-3 flex flex-row flex-wrap w-full desktop:w-9/12 desktop:min-w-[1000px] tablet:w-full h-fit">
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
+      <Card />
     </div>
   );
 };
