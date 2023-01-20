02e4bb3f2b056747f78fd6c9e61f187c2c2a19bb
headring <myhakiki@gmail.com>
2023-01-20 18:45:35 +0900

    feat: 글작성 axios 완성

--- client/apis/postWrite.tsx
+++ client/apis/postWrite.tsx
@@ -1,29 +1,17 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 
 const postWrite = async (object: FormData) => {
   const url = "/lesson/registration";
-  const baseURL = "https://88f0-182-226-233-7.jp.ngrok.io";
-  const Authorization =
-    "Baerer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6WyJVU0VSIl0sImlkIjoxMSwiZW1haWwiOiJtYW1lMUBnbWFpbC5jb20iLCJzdWIiOiJtYW1lMUBnbWFpbC5jb20iLCJpYXQiOjE2NzQxNDA5MTIsImV4cCI6MTY3NDE0MjcxMn0.jp1uw8Tg-T3HC5FkjgfLY2q4tc0WscsrOWTUJfdvy84xFhgNOQGzehZDG5uPM590TjmInah9skGEiMYwEcYz2A";
-  const RefreshToken =
-    "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtYW1lMUBnbWFpbC5jb20iLCJpYXQiOjE2NzQxNDA5MTIsImV4cCI6MTY3NDIyNzMxMn0.m0wuHXhRtF0SepkQe17HNHc43T4Qj4BL-3dkqsBmYsIWM4rQGkJvunVL4PBcAT0wuPvL8ufWtwMEScAhAm7j1g";
 
-  await axios
-    .post(url, object, {
-      baseURL,
-      headers: {
-        "ngrok-skip-browser-warning": "63490",
-        "Content-Type": "multipart/form-data",
-        Authorization,
-        RefreshToken,
-      },
-    })
-    .then(response => {
-      console.log(response);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  return await axios.post(url, object, {
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+      "Content-Type": "multipart/form-data",
+      "ngrok-skip-browser-warning": "63490",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      RefreshToken: localStorage.getItem("refresh_token"),
+    },
+  });
 };
 
 export default postWrite;
