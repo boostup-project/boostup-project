8d6f05f8d5bbe2101d62c61851d5569d85f3ace8
quartz614 <quartz614@naver.com>
2023-01-23 20:55:21 +0900

    refactor: lesson 수정  리팩터링 중

--- server/src/main/java/com/codueon/boostUp/domain/lesson/dto/GetLessonInfo.java
+++ server/src/main/java/com/codueon/boostUp/domain/lesson/dto/GetLessonInfo.java
@@ -1,9 +1,13 @@
 package com.codueon.boostUp.domain.lesson.dto;
 
+import com.codueon.boostUp.domain.lesson.entity.CareerImage;
 import com.codueon.boostUp.domain.lesson.entity.LessonInfo;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -13,7 +17,7 @@ public class GetLessonInfo {
    private String personality;
    private String detailCost;
    private String detailLocation;
-
+   private List<Long> careerImage;
    @Builder
     public GetLessonInfo(LessonInfo lessonInfo) {
         this.introduction = lessonInfo.getIntroduction();
@@ -21,5 +25,8 @@ public class GetLessonInfo {
         this.personality = lessonInfo.getPersonality();
         this.detailCost = lessonInfo.getCosts();
         this.detailLocation = lessonInfo.getFavoriteLocation();
+        this.careerImage = lessonInfo.getCareerImages().stream()
+                .map(CareerImage::getId)
+                .collect(Collectors.toList());
     }
 }
