b9aa9a669cecc810f74b28762e02a91f37c1524d
headring <myhakiki@gmail.com>
2023-01-24 18:37:23 +0900

    feat: 로그아웃 이벤트 생성(로컬 클리어 및 persist정보 반전)

--- client/components/Header/Header.tsx
+++ client/components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useRecoilState } from "recoil";
-import { logUser } from "atoms";
 import { SubmitHandler } from "react-hook-form";
 import { textColor } from "assets/color/color";
 import { IconExit, IconMagnify } from "assets/icon/";
@@ -9,7 +8,10 @@ import { powerWriteModal } from "atoms/main/mainAtom";
 import Image from "next/image";
 import Logo from "../../public/images/logo.png";
 import Link from "next/link";
-import WriteModal from "components/createModal/WriteModal";
+import { logUser } from "atoms/auth/authAtom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
 
 interface SearchData {
   name: string;
@@ -21,7 +23,7 @@ interface SearchData {
 }
 
 const Header = () => {
-  const [log, setLog] = useRecoilState(logUser);
+  const [log, setLog] = useRecoilState<boolean>(logUser);
   const [seek, setSeek] = useState(false);
   const [isPowerWrite, setIsPowerWrite] = useRecoilState(powerWriteModal);
 
@@ -33,12 +35,21 @@ const Header = () => {
     setIsPowerWrite(prev => !prev);
   };
 
+  const logOutEvent = () => {
+    localStorage.clear();
+    setLog(prev => !prev);
+    toast.success("로그아웃이 되었습니다", {
+      autoClose: 1500,
+      position: toast.POSITION.TOP_RIGHT,
+    });
+  };
+
   return (
     <>
       <header className="bg-bgColor font-SCDream5 w-full pt-4 fixed top-0 z-[1] h-fit desktop:w-3/4 desktop:min-w-[1000px] desktop:h-[87px] desktop:mt-0">
         <nav className="w-full h-full flex tablet:justify-center tablet:items-center desktop:justify-between">
           <div className="w-1/2 flex justify-start items-center ml-4 tablet:h-[40px] desktop:hidden">
-            <div className="w-8 tablet:w-[34.5px]">
+            <div className="w-8 tablet:w-[34.5px]" onClick={logOutEvent}>
               <IconExit />
             </div>
           </div>
@@ -72,10 +83,7 @@ const Header = () => {
               {log ? (
                 <>
                   <div className="min-w-fit mr-8 py-1">마이페이지</div>
-                  <div
-                    className="min-w-fit mr-8 py-1"
-                    onClick={e => setLog(!log)}
-                  >
+                  <div className="min-w-fit mr-8 py-1" onClick={logOutEvent}>
                     로그아웃
                   </div>
                   <div
@@ -87,7 +95,7 @@ const Header = () => {
                 </>
               ) : (
                 <>
-                  <div className="mr-8" onClick={e => setLog(!log)}>
+                  <div className="mr-8">
                     <Link href="/login">로그인</Link>
                   </div>
                   <div>
