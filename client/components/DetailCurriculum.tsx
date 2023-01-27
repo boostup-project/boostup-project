df266e11ced345b65f192dd5d1a28896b89ad649
headring <myhakiki@gmail.com>
2023-01-27 16:18:28 +0900

    feat: api통신을 이용하여 진행정보 렌더링

--- client/components/DetailCurriculum.tsx
+++ client/components/DetailCurriculum.tsx
@@ -1,5 +1,59 @@
-const DetailCurriculum = () => {
-  return <div>DetailCurriculum</div>;
+import { AxiosResponse } from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
+const ReactQuill = dynamic(() => import("react-quill"), {
+  ssr: false,
+});
+
+interface Props {
+  curData: AxiosResponse<any, any> | undefined;
+}
+
+const dummy =
+  "writing. \n ## Title2 \n title none asdlajsld asldkjalskdj laskdjlaskjd laskdj laksjdlaksj ldkajsdl \n adasdas \n asdkajlsdkj \n aksdjhaksjdhaksj \n akjdhaksjdhaksjhd \n asdjkhaskdjhak \n asdjhaksdjhaksjdh \nkajsdhaksjdh \nqw9iepoqwiepqwoiep \n qweoiqpwoeipqwoei";
+
+const DetailCurriculum = ({ curData }: Props) => {
+  const [textData, setTextData] = useState<string>("");
+  const [quillText, setQuillText] = useState<string>("");
+  console.log("text", textData);
+  useEffect(() => {
+    if (curData) {
+      setTextData(curData.data.curriculum);
+    }
+  }, [curData]);
+  const onChange = (e: string) => {
+    setQuillText(e);
+  };
+  return (
+    <div className="w-full h-full p-6 text-base">
+      {/* <div className="font-SCDream5">진행방식</div> */}
+      {/* <div>{textData}</div> */}
+      <ReactQuill onChange={e => onChange(e)} />
+      <div
+        className="quillBody"
+        dangerouslySetInnerHTML={{ __html: quillText }}
+      />
+    </div>
+  );
 };
 
 export default DetailCurriculum;
+
+// <div className="detailCurriculum w-full h-fit">
+//   <div data-color-mode="light"></div>
+// </div>
+// const MDEditor = dynamic(() => import("@uiw/react-md-editor"), {
+//   ssr: false,
+// });
+// {
+//   /* <MDEditor
+//             preview="preview"
+//             value={dummy}
+//             fullscreen={false}
+//             // onHeightChange={100%}
+//             // onHeightChange={}
+//             // height={`100%`}
+//           /> */
+// }
---
+++ client/hooks/detail/useGetCurriculum.tsx
@@ -0,0 +1,16 @@
+import getCurriculum from "apis/detail/getCurriculum";
+import { useQuery } from "react-query";
+
+const useGetCurriculum = (lessonId: number) => {
+  return useQuery(["get/Extra"], () => getCurriculum(lessonId), {
+    enabled: false,
+    staleTime: 5 * 60000,
+    onSuccess: res => {
+      console.log("sucess");
+    },
+    onError: res => {
+      console.log("failed");
+    },
+  });
+};
+export default useGetCurriculum;
