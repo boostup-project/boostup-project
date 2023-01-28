db32609fed1f7f7c41d1b47d84e87ae6d3282be9
headring <myhakiki@gmail.com>
2023-01-29 02:55:33 +0900

    feat: markdown render 성공

--- client/components/DetailCurriculum.tsx
+++ client/components/DetailCurriculum.tsx
@@ -1,59 +1,56 @@
 import { AxiosResponse } from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
-import dynamic from "next/dynamic";
-import "react-quill/dist/quill.snow.css";
-const ReactQuill = dynamic(() => import("react-quill"), {
-  ssr: false,
-});
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 interface Props {
   curData: AxiosResponse<any, any> | undefined;
 }
 
-const dummy =
-  "writing. \n ## Title2 \n title none asdlajsld asldkjalskdj laskdjlaskjd laskdj laksjdlaksj ldkajsdl \n adasdas \n asdkajlsdkj \n aksdjhaksjdhaksj \n akjdhaksjdhaksjhd \n asdjkhaskdjhak \n asdjhaksdjhaksjdh \nkajsdhaksjdh \nqw9iepoqwiepqwoiep \n qweoiqpwoeipqwoei";
+const dummy = `## Title2 
+asdasd asdasdas
+asdasd asdas
+1. asdasd
+- Foo
+- Bar
+  * Baz
+    + Nope?
+  * Hai?
+`;
 
 const DetailCurriculum = ({ curData }: Props) => {
   const [textData, setTextData] = useState<string>("");
-  const [quillText, setQuillText] = useState<string>("");
-  console.log("text", textData);
+  const [isEdit, setIsEdit] = useState(false);
   useEffect(() => {
     if (curData) {
       setTextData(curData.data.curriculum);
     }
   }, [curData]);
-  const onChange = (e: string) => {
-    setQuillText(e);
+  const modalOpen = () => {
+    setIsEdit(prev => !prev);
   };
   return (
-    <div className="w-full h-full p-6 text-base">
-      {/* <div className="font-SCDream5">진행방식</div> */}
-      {/* <div>{textData}</div> */}
-      <ReactQuill onChange={e => onChange(e)} />
-      <div
-        className="quillBody"
-        dangerouslySetInnerHTML={{ __html: quillText }}
-      />
-    </div>
+    <>
+      {isEdit && <div>hi</div>}
+      <div className="w-full h-full p-6 text-base">
+        <div className="flex justify-between mb-3">
+          <div className="font-SCDream5">진행방식</div>
+          <div
+            onClick={modalOpen}
+            className="text-pointColor font-SCDream3 cursor-pointer hover:underline"
+          >
+            edit
+          </div>
+        </div>
+        <div className="bg-markBgColor rounded-xl p-4">
+          <div className="prose">
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{dummy}</ReactMarkdown>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default DetailCurriculum;
-
-// <div className="detailCurriculum w-full h-fit">
-//   <div data-color-mode="light"></div>
-// </div>
-// const MDEditor = dynamic(() => import("@uiw/react-md-editor"), {
-//   ssr: false,
-// });
-// {
-//   /* <MDEditor
-//             preview="preview"
-//             value={dummy}
-//             fullscreen={false}
-//             // onHeightChange={100%}
-//             // onHeightChange={}
-//             // height={`100%`}
-//           /> */
-// }
