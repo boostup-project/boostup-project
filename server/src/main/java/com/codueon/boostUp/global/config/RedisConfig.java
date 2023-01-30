2476fedd6630ea00210c1ecd33cf10a6483cd8a4
mozzi327 <mozzi327@gmail.com>
2023-01-30 09:50:40 +0900

    fix: redis cluster 설정 변경

--- server/src/main/java/com/codueon/boostUp/global/config/RedisConfig.java
+++ server/src/main/java/com/codueon/boostUp/global/config/RedisConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -44,7 +45,9 @@ public class RedisConfig {
      */
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
+        clusterConfiguration.clusterNode(redisHost, redisPort);
+        return new LettuceConnectionFactory(clusterConfiguration);
     }
 
     /**
