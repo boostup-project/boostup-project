35432c32874649f51a57bb96cb0d567d8c656097
headring <myhakiki@gmail.com>
2023-01-30 14:34:31 +0900

    refactor: 토큰 유무에 따른 메인페이지 카드 불러우기

--- client/apis/card/getMainCard.tsx
+++ client/apis/card/getMainCard.tsx
@@ -1,14 +1,25 @@
 import axios from "axios";
 
 const getMainCard = async () => {
-  return await axios.get(`/lesson`, {
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
-    headers: {
-      "content-Type": `application/json`,
-      "ngrok-skip-browser-warning": "69420",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-      RefreshToken: localStorage.getItem("refresh_token"),
-    },
-  });
+  const author = localStorage.getItem("token");
+
+  if (author) {
+    return await axios.get(`/lesson`, {
+      baseURL: process.env.NEXT_PUBLIC_API_URL,
+      headers: {
+        "content-Type": `application/json`,
+        "ngrok-skip-browser-warning": "69420",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+  } else {
+    return await axios.get(`/lesson`, {
+      baseURL: process.env.NEXT_PUBLIC_API_URL,
+      headers: {
+        "content-Type": `application/json`,
+        "ngrok-skip-browser-warning": "69420",
+      },
+    });
+  }
 };
 export default getMainCard;
