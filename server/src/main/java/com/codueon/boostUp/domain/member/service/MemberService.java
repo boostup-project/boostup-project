233ae6e75d9dccb340f8664c5ede35236de6cc7f
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2023-02-01 15:46:58 +0900

    refactor: 사용자 프로필 수정 시 디폴트 이미지를 제외한 기존 S3 이미지 삭제 (#498)
    * refactor: 사용자 프로필 수정 시 디폴트 이미지를 제외한 기존 S3 이미지 삭제
    
    * Update MemberService.java
    
    ---------
    
    Co-authored-by: LeeGoh <“gyuleegoh@gmail.com”>

--- server/src/main/java/com/codueon/boostUp/domain/member/service/MemberService.java
+++ server/src/main/java/com/codueon/boostUp/domain/member/service/MemberService.java
@@ -31,7 +31,6 @@ public class MemberService {
     private final FileHandler fileHandler;
     private final AwsS3Service awsS3Service;
 
-
     @Value("${default.image.address}")
     private String defaultImageAddress;
 
@@ -130,11 +129,16 @@ public class MemberService {
         if (file.isEmpty() && name.getName().isEmpty()) return null;
 
         Member findMember = memberDbService.ifExistsReturnMember(memberId);
-        UploadFile uploadFile = fileHandler.uploadFile(file);
+//        UploadFile uploadFile = fileHandler.uploadFile(file);
 
         if (!file.isEmpty()) {
-//            String dir = "memberImage";
-//            UploadFile uploadFile = awsS3Service.uploadfile(file, dir);
+            String dir = "memberImage";
+            UploadFile uploadFile = awsS3Service.uploadfile(file, dir);
+
+            if (!findMember.getMemberImage().getFilePath().equals(defaultImageAddress)){
+                awsS3Service.delete(findMember.getMemberImage().getFileName(), dir);
+            }
+
             MemberImage memberImage = MemberImage.builder()
                     .filePath(uploadFile.getFilePath())
                     .fileName(uploadFile.getFileName())
