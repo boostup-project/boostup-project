686f3f2ee147f2a830dd854221c9767e62898053
mozzi327 <mozzi327@gmail.com>
2023-02-07 15:15:09 +0900

    feat: 회원별 채팅방 구독 기능 구현

--- server/src/main/java/com/codueon/boostUp/domain/chat/dto/PostMessage.java
+++ server/src/main/java/com/codueon/boostUp/domain/chat/dto/PostMessage.java
@@ -6,18 +6,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor
 public class PostMessage {
+
+    @NotNull
+    private Long memberId;
     @NotBlank
     private Long chatRoomId;
+    @NotBlank
+    private String memberImage;
     @NotBlank
     private String messageContent;
 
     @Builder
-    public PostMessage(Long chatRoomId, String messageContent) {
+    public PostMessage(Long memberId,
+                       Long chatRoomId,
+                       String memberImage,
+                       String messageContent) {
+        this.memberId = memberId;
         this.chatRoomId = chatRoomId;
+        this.memberImage = memberImage;
         this.messageContent = messageContent;
     }
 }
