3aee8e3e716e59165882bc5725c022bdf79c7750
headring <myhakiki@gmail.com>
2023-02-10 02:35:54 +0900

    refactor: retry, cache, stale 삭제

--- client/hooks/detail/useGetExtra.tsx
+++ client/hooks/detail/useGetExtra.tsx
@@ -3,9 +3,6 @@ import { useQuery } from "@tanstack/react-query";
 
 const useGetExtra = (lessonId: number) => {
   return useQuery(["get/Extra"], () => getExtra(lessonId), {
-    staleTime: 5 * 60000,
-    cacheTime: 5 * 60000,
-    retry: 10,
     enabled: false,
     onSuccess: res => {
       console.log("sucess");
