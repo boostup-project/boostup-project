2431b21c98fa3b1e479aad748a1f0bc2d61580cf
SangHyun Kim <myhakiki@gmail.com>
2023-02-17 22:26:24 +0900

    chore: next config 수정

--- client/next.config.js
+++ client/next.config.js
@@ -17,6 +17,12 @@ const nextConfig = {
     return [
       {
         source: "/apis/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
         destination: BASE_URL + "/:path*",
       },
     ];
