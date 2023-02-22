d998747185f7df474bfd06f0919ce26bf06809a5
skynolimit <sky221b9@gmail.com>
2023-02-22 14:43:24 +0900

    featGoogleOauth2

--- client/pages/api/auth/[...nextauth].ts
+++ client/pages/api/auth/[...nextauth].ts
@@ -5,8 +5,8 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   secret: process.env.JWT_SECRET,
