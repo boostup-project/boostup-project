9eeb681bddae896b4e2ca938b646dac2a8c02e8d
skynolimit <sky221b9@gmail.com>
2023-02-22 14:33:31 +0900

    featGoogleOauth

--- client/pages/api/auth/[...nextauth].ts
+++ client/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,13 @@
-// pages/api/auth/[...nextauth].ts
+// Next.js 프로젝트의 pages/api/auth/[...nextauth].js
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-// export default NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     }),
-//   ],
-// });
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.JWT_SECRET,
+});
